ce2160570b0c4d3ea2b590cbf24e0889f2d64b0a
Saravanakumaar Jeyabalan <jsarava@gmail.com>
2020-03-21 12:05:17 +0530

    Added update function

--- hexagon-mesh/hexagon-mesh.js
+++ hexagon-mesh/hexagon-mesh.js
@@ -15,11 +15,13 @@ tgs.widgets.Hexagon = (function (d3) {
         const hexagonHorCount = Math.floor(graphWidth / hexDia);
         const hexagonVerCount = Math.floor(graphHeight / hexDia);
 
+        let hexagonGroup = null;
+        let hexagonMesh = null;
         let x1 = 0;
         let y1 = hexRadius;
         let x2, y2, x3, y3, x4, y4, x5, y5, x6, y6;
         let lineGenerator = d3.line();
-        let hexagonMesh = new Array();
+        let meshSpec = new Array();
 
         const _prepareData = function () {
             let hexagonCounter = 0;
@@ -40,16 +42,16 @@ tgs.widgets.Hexagon = (function (d3) {
                     x6 = x2;
                     y6 = y5;
 
-                    hexagonMesh.push([[x1, y1], [x2, y2], [x3, y3], [x4, y4], [x5, y5], [x6, y6]]);
+                    meshSpec.push([[x1, y1], [x2, y2], [x3, y3], [x4, y4], [x5, y5], [x6, y6]]);
 
                     y1 = y5 + (hexRadius * Math.sin(Math.PI/3));
                 }
                 if (i % 2 === 0) {
-                    x1 = hexagonMesh[hexagonMesh.length - hexagonVerCount][4][0];
-                    y1 = hexagonMesh[hexagonMesh.length - hexagonVerCount][4][1];
+                    x1 = meshSpec[meshSpec.length - hexagonVerCount][4][0];
+                    y1 = meshSpec[meshSpec.length - hexagonVerCount][4][1];
                 } else {
-                    x1 = hexagonMesh[hexagonMesh.length - hexagonVerCount][2][0];
-                    y1 = hexagonMesh[hexagonMesh.length - hexagonVerCount][2][1];
+                    x1 = meshSpec[meshSpec.length - hexagonVerCount][2][0];
+                    y1 = meshSpec[meshSpec.length - hexagonVerCount][2][1];
                 }
             }
         }
@@ -57,18 +59,20 @@ tgs.widgets.Hexagon = (function (d3) {
         const _render = function (data) {
             _prepareData();
 
-            let hexagonGroup = d3.select(containerId)
+            hexagonMesh = d3.select(containerId)
                 .append("svg")
-                    .attr("class", "hexagon")
+                    .attr("class", "hexagon-mesh")
                     .attr("width", graphWidth)
                     .attr("height", graphHeight)
                     .append("g")
+                        .attr("class", "mesh-component")
                         .attr("transform", "translate(0, 0)")
                         .selectAll("g")
-                            .data(hexagonMesh)
-                            .enter()
-                            .append("g")
-                                .attr("class", "hexagon-group");
+                            .data(meshSpec);
+
+            hexagonGroup = hexagonMesh.enter()
+                .append("g")
+                .attr("class", "hexagon-group");
 
             hexagonGroup.append("path")
                 .attr("class", "hexagon")
@@ -82,12 +86,34 @@ tgs.widgets.Hexagon = (function (d3) {
                 });
 
             hexagonGroup.append("text")
+                .attr("class", "label")
                 .attr("x", (d, i) => d[0][0]) // + (d[3][0] - d[0][0]) / 2)
                 .attr("y", (d, i) => d[1][1] + 5 + (d[4][1] - d[1][1]) / 2)
                 .style("fill", "#000000")
                 .style("text-anchor", "start")
                 .attr("textLength", hexDia)
                 .attr("lengthAdjust", "spacingAndGlyphs")
+                .text((d, i) => {
+                    let textValue = data[i];
+                    if (textValue)
+                        return textValue["text"];
+                    else
+                        return "";
+                });
+        }
+
+        const _update = function(data) {
+            //It is assumed that the Hexagon Mesh is not changed however the Hexagon's title and color changed.
+            d3.selectAll(".hexagon").transition().duration(1000).attr("fill", (d, i) => {
+                console.log(i + "|" + d);
+                let fillValue = data[i];
+                if (fillValue)
+                    return fillValue["fill"];
+                else
+                    return "none";
+            });
+            
+            d3.selectAll(".label").transition().duration(1000)
                 .text((d, i) => {
                     let textValue = data[i];
                     if (textValue)
